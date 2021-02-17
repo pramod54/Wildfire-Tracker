c4994c8907638583cd1002917fa51f3016ceeb97
prince chamoli <princechamoli22@gmail.com>
2021-02-17 19:51:52 +0530

    Second commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 import LocationInfoBox from './LocationInfoBox'
-
 // define constants
 //const NATURAL_EVENT_WILDFIRE = 8;
 
@@ -13,13 +12,14 @@ const Map = ({ eventData, center, zoom }) => {
         // 
         if(ev.categories[0].title === "Wildfires") {
             return <LocationMarker name={ev.categories[0].title} key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+            return null;
         }
     })
 
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyD7A_0WZmQ-2VFkNA2s-K4lwlPXUfmN8BQ' }}
+                bootstrapURLKeys={{ key: process.env.JWT_SECRET }}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
