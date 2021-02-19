b36b7e397c525a3f5b8cefdababf7f1a728bd142
prince chamoli <princechamoli22@gmail.com>
2021-02-19 12:27:55 +0530

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert'
 const Header = () => {
     return (
         <header className="header">
-            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered By NASA)</h1>
+            <h1><Icon icon={locationIcon} /> Nature Calamity (Powered By NASA)</h1>
         </header>
     )
 }
